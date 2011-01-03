c866647af6c41844a8d6a53310d0f56a12fa4ded
chris.m.john <chris.m.john@btinternet.com>
2011-01-03 18:12:01 +0000

    renamed directory structure to com.armchairfun.poker.*

--- src/main/java/com/armchairfun/common/HandRanks.java
+++
@@ -1,8 +0,0 @@
-package com.armchairfun.common;
-
-public class HandRanks 
-{
-// TODO store all the hands here from best to worst, to determine the winner(s)
-    
-    
-}
--- src/main/java/com/armchairfun/common/UserConstants.java
+++
@@ -1,9 +0,0 @@
-package com.armchairfun.common;
-
-
-public class UserConstants {
-
-	// $500 dollar play money to start with
-	public static final int STARTING_PLAY_CASH = 500;
-
-}
