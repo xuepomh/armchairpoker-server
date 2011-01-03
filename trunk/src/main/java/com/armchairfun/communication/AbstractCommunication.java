868da4115792436e3c28c12341d7d2e566d84672
chris.m.john@btinternet.com <chris.m.john@btinternet.com@3a8c7e44-2ef2-ee9b-4f76-cfea73f04e04>
2011-01-03 18:12:30 +0000

    renamed directory structure to com.armchairfun.poker.*

--- trunk/src/main/java/com/armchairfun/communication/AbstractCommunication.java
+++
@@ -1,9 +0,0 @@
-package com.armchairfun.communication;
-
-public class AbstractCommunication implements ICommunication {
-
-	public String toXML() {
-		//TODO
-		return "<xml>not implemented yet!</xml>";
-	}
-}
